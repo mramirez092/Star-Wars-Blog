e14999b30ea94b5425ca7963b5063a7e4ae3da06
mramirez092 <miguelramirez2210@gmail.com>
2023-04-18 16:52:14 +0000

    Estructura incial, personajes

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,15 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import swlogo from "../../img/Star_Wars_Logo.png";
+import stylesheet from "../../styles/navbar.css";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
+		<nav className="navbar navbar-light bg-dark mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<span className="navbar-brand text-light m-3 h1">Star Wars Blog</span>
 			</Link>
+			<div className="logo">
+				<img src={swlogo}/>
+			</div>
 			<div className="ml-auto">
 				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
+					<button className="btn btn-secondary m-3">Check the Context in action</button>
 				</Link>
 			</div>
 		</nav>
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,43 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import { useState, useEffect } from "react";
 import "../../styles/home.css";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+
+const [data, setData] = useState([]);
+
+useEffect (() => {
+	fetch("https://www.swapi.tech/api/people")
+	.then((response) => response.json())
+	.then((data) => {
+    setData(data.results)
+    console.log(data)
+  })
+	.catch(error => console.error(error))
+}, []);
+
+return (
+    <div className="container-fluid">
+      <div className="blog-title text-light m-3 pb-4">
+        <h1>Characters</h1>
+      </div>
+      
+      <div className="row flex-row flex-nowrap" id="scroll-container">
+          {data.map((character, index) => (
+              <div key={index} className="col">
+                <div className="card">
+                  <h4>{character.name}</h4>
+                  <p>
+                    Gender
+                  </p>
+                  <button data-text="Awesome" class="button">
+                     <span class="actual-text">&nbsp;Learn More!&nbsp;</span>
+                     <span class="hover-text" aria-hidden="true">&nbsp;Learn More!&nbsp;</span>
+                  </button>
+                </div>
+              </div>
+        ))}
+      </div>
+    </div>
+  );
+};
